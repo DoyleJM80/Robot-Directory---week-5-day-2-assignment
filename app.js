f682697cff05ef6dabd700dd6561aef4bc57a084
Jamie Doyle <thedoyles922@users.noreply.github.com>
2017-06-20 16:15:11 -0400

    added elements to html

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 var express = require('express');
 var mustacheExpress = require('mustache-express');
 var app = express();
-var data = require('data.js');
+var data = require('./data.js');
+
+app.use('/public', express.static('public'));
 
 app.engine('mustache', mustacheExpress());
 app.set('view engine', 'mustache');
 app.set('views', './views');
 
-app.get('/', function (req, res) {
-  res.send(data);
+app.get('/index', function (req, res) {
+  var context = data;
+  res.render('index', context);
 });
 
 app.listen(3000, function () {
   console.log('listening');
 });
+//
